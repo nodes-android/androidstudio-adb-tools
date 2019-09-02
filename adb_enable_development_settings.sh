0150f7e2cf78b8485eee4193fc96561d1894c166
maru <maru@nodesagency.com>
2019-09-02 11:12:24 +0200

    Add developer option scripts for enabling and opening

---
+++ adb_enable_development_settings.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Enables Android Developer Settings
+
+adb shell settings put global development_settings_enabled 1
---
+++ adb_open_development_settings.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Opens Android Developer Settings
+
+adb shell am start -a com.android.settings.APPLICATION_DEVELOPMENT_SETTINGS
